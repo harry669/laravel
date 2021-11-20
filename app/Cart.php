9e10afae814888932d772d26c9c0ac2420490256
Prathmesh <manasdhumal159@gmail.com>
2021-11-20 21:35:40 +0530

    Cart User Storage

---
+++ app/Cart.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+
+      protected $fillable = [
+        'cart_id','user_id',
+      ];
+
+}
--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Cart;
 use App\Product;
 use App\Price;
+use App\Cart as CartStorage;
 class CartController extends Controller
 {
     //
         public function test()
         {
         	 $rowId= '8cbf215baa3b757e910e5305ab981172';
-            //Cart::add('293ad', 'Product 1', 1, 9.99, 550);
+           // $cart_obj= Cart::add('293ad', 'Product 1', 1, 9.99, 550);
            // Cart::update($rowId, 2);
         	//$cart_total= Cart::total();
             //$cart= Cart::content();
-        	 Cart::destroy();
+        	Cart::destroy();
         	 $cart= Cart::content();
         	 dd($cart);
         }
@@ -30,10 +32,10 @@ class CartController extends Controller
             $product_quantity= $this->getQuantity($product_quantity);
             $product_weight= $this->getweight(550);
             $cart_obj = Cart::add($product_id,$product->product_name,$product_quantity,$product_price,$product_weight);
-            $cart_quantity= $cart_obj->qty;
+            $save_cart= $this->saveToCart($cart_obj);
+            //$cart_quantity= $cart_obj->qty;
              return back()
-            ->with('success', 'Item Added in cart')
-            ->with('qty', $cart_obj);
+            ->with('success', 'Item Added in cart');
         }
 
         public function getPrice($product_id)
@@ -57,6 +59,24 @@ class CartController extends Controller
         	 $cart_data= Cart::content();
         	 return response()
             ->json(['cart' => $cart_data]);
+        }
+
+        public function saveToCart($cart_obj)
+        {
+             if (Auth::check()) {
+
+                    //user is logged in 
+             	   $cart_id= $cart_obj->rowId;
+             	    $User_Cart = CartStorage::updateOrCreate(
+                          ['cart_id' => $cart_id],
+                          ['user_id' => Auth::id()]
+                       );
+
+
+             	  return true;
+              }
+
+              return false;
         }
 
 
